e428e4541ab90525bed6001786a2b909b7a93657
bapt <baptiste.gauthier@laplateforme.io>
2020-12-04 14:39:07 +0100

    style index

---
+++ include/header_disconnect.php
@@ -0,0 +1,10 @@
+<nav class="contenu_header">
+    <ul>
+        <li><a href="index.php"> Accueil </a></li>
+        <li><a href="pages/inscription.php"> Inscription </a></li>
+        <li><a href="pages/connexion.php"> Connexion </a></li>
+    </ul>
+    <div>
+        <img src="images/logo.svg" alt="logo">
+    </div>
+</nav>
